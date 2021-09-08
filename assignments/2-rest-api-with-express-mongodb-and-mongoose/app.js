c1310432a240da7a8d0bdbaf23eb82cff40d9e78
shepard_liu <1057927664@qq.com>
2021-09-08 18:00:04 +0800

    complete

---
+++ assignments/2-rest-api-with-express-mongodb-and-mongoose/app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//Routers
+var dishRouter = require('./routers/dishRouter');
+var promoRouter = require('./routers/promoRouter');
+var leaderRouter = require('./routers/leaderRouter');
+
+//MongoDB
+var mongoose = require('mongoose');
+
+//Connect database server
+mongoose.connect('mongodb://localhost:27017/confusion').then(() => {
+  console.log('Connected to Mongo DB');
+}).catch((err) => {
+  console.log(err);
+});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//use Routers
+app.use('/dishes', dishRouter);
+app.use('/promos', promoRouter);
+app.use('/leaders', leaderRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
